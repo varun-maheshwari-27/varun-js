2f3989e2623fb4f2d90a7e7fbb982a5f80ab2588
varun-maheshwari-27 <143994214+varun-maheshwari-27@users.noreply.github.com>
2023-12-06 07:52:46 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "geekytech2002@gmail.com"
+var accountpassword = "12345"
+accountCity = "Vadodara"
+let accountRegion;
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
+
+accountEmail = "varun@varun.com"
+accountpassword = "223344"
+accountCity = "Ahmedabad"
+
+// Dont prefer to use 'var or dont even leave blank. Always write Let'
+//
+
+console.table([accountId, accountEmail, accountpassword, accountCity, accountRegion]);
