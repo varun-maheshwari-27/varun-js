77c348105dc3a69bb904bd84e580754fc315c1fc
varun-maheshwari-27 <143994214+varun-maheshwari-27@users.noreply.github.com>
2023-12-07 10:48:31 +0000

    datatype summary

--- 01_basics/05_datatypes-summary.js
+++ 01_basics/05_datatypes-summary.js
@@ -32,9 +32,31 @@ const myfunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof myfunction);
+// console.log(typeof myfunction);
 
 // Javascript is dynamically typed. Why? Because it doesn't need to be specified. 
 // For eg: const score = False. Here we've not described it as boolean but still Javascript recognises it.
 
+// -------------------------------------------------------------------------------------------------------------------------
+// Primitive Datatypes - Stack Memory 
+let username1 = "Varun"
+let username2 = username1
+username2 = "Akansha"
 
+// Non-Primitive Datatypes - Heap Memory
+let username3 = {
+    email: "varun@maheshwari.com",
+    upi: "varun@hdfc"
+}
+
+let akansha = username3
+
+akansha.email = "akansha@varun.com"
+
+console.log(akansha.email);
+console.log(username3.email);
+
+
+
+// console.log(username1);
+// console.log(username2);
